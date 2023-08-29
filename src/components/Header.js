808683e3600eba50c71b07e4aea7b907423ecc5e
aredom <leakearedom23@gmail.com>
2023-08-29 13:26:38 +0300

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,8 @@ import "./Header.css";
 import { Link } from "react-router-dom";
 import nberlogo from "../Images/niber-logo.png";
 import { FaChevronDown, FaHandHoldingHeart, FaToolbox } from "react-icons/fa";
-import { FaBars } from "react-icons/fa";
-import { useState } from "react";
 
 function Header() {
-  const [open, setOpen] = useState(false);
   return (
     <header className="header">
       <div>
@@ -17,51 +14,41 @@ function Header() {
       </div>
 
       <nav>
-        {open && (
-          <ul className="navbar-menu">
+        <ul className="navbar-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <ul className="Menu">
             <li>
-              <Link to="/">Home</Link>
-            </li>
-            <ul className="Menu">
-              <li>
-                <a>
-                  Programs
-                  <FaChevronDown className="fachek" />
-                </a>
+              <a href="#!">
+                Programs
+                <FaChevronDown className="fachek" />
+              </a>
 
-                <ul className="programs-submenu">
-                  <li>
-                    <FaHandHoldingHeart />
+              <ul className="programs-submenu">
+                <li>
+                  <FaHandHoldingHeart />
+                  <Link to="/humanitarian">Humanitarian</Link>
+                </li>
 
-                    <Link to="/humanitarian">Humanitarian</Link>
-                  </li>
-
-                  <li>
-                    <FaToolbox />
-
-                    <Link to="/programs">Development</Link>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            {/* <li>
-            <Link to="/programs">programs</Link>
-           </li> */}
-
-            <li>
-              <Link to="/donate">Donate</Link>
-            </li>
-            <li>
-              <Link to="/LoginPage">Subscribe</Link>
+                <li>
+                  <FaToolbox />
+                  <Link to="/programs">Development</Link>
+                </li>
+              </ul>
             </li>
           </ul>
-        )}
-        <button onClick={() => setOpen(!open)} className="humbtn">
-          <FaBars />
-        </button>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+
+          <li>
+            <Link to="/donate">Donate</Link>
+          </li>
+          <li>
+            <Link to="/LoginPage">Subscribe</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
